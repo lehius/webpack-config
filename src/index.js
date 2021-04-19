91ddad4b2542b8930baff3eada69fc8b7474c124
lehius <lehius@bk.ru>
2021-04-19 20:20:46 +0300

    add css, sass, postcss, hotreloading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import "./styles/index.scss";
+import {createLogger} from "./common";
+
+const logger = createLogger('foo');
+
 const foo = {
   field1: 2,
   field2: 1,
   field3: 4,
 };
+logger.log(`create foo: ${JSON.stringify(foo)}`);
 
 const fooGa = {
   ...foo,
   field4: 3,
 };
 
-console.log(foo);
-console.log(fooGa);
+logger.log(`create fooGa: ${JSON.stringify(fooGa)}`);
+
+console.dir(logger.getLogs());
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+const target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
 
 module.exports = {
   mode,
+  target,
 
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          // options: {
-          //   presets: ["@babel/preset-env"]
-          // }
         },
       },
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
 };
